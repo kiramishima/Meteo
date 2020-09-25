ed4777fb6e5aebc61756b5a5755fbd9a4df04509
Paul <kiramishima@outlook.com>
2020-09-24 23:53:16 -0500

    Update models

--- lib/repository/api_client.dart
+++ lib/repository/api_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:meteo/models/CurrentWeatherResponse.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'api_client.g.dart';
@@ -8,4 +9,13 @@ abstract class APIClient {
   factory APIClient(Dio dio, {String baseUrl}) {
     return _APIClient(dio, baseUrl: baseUrl);
   }
+
+  @GET("weather")
+  @DioResponseType(ResponseType.json)
+  Future<CurrentWeatherResponse> getCurrentWeather(
+      @Query("q") String q
+      @Query("units") String units,
+      @Query("lang") String lang,
+      @Query("appid") String appId
+  );
 }
